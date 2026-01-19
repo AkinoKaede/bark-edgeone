c5bb2b1e6c0a722aca4f5858fc460f7cc8ad5d4b
Kaede Akino <i@akinokae.de>
2026-01-19 11:42:11 +0800

    feat: better helloworld

--- edge-functions/helloworld-edge/index.ts
+++ edge-functions/helloworld-edge/index.ts
@@ -1,8 +1,11 @@
-export function onRequest(context: any): Response {
+export async function onRequestGet({ request, params, env }: any): Promise<Response> {
   const json = JSON.stringify({
     "code": 0,
     "message": "Hello World"
   });
+
+  // // @ts-ignore KV binding injected by EdgeOne Pages
+  // await KV_STORAGE.put('test_key', 'test_value');
   return new Response(json, {
     headers: {
       'content-type': 'application/json',
